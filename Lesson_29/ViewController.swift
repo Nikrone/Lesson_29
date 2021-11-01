0e2048b692fd04183e635c07ef9a25f42b563c4b
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-11-01 19:35:56 +0300

    feat: added implementation KeyChain

--- Lesson_29/ViewController.swift
+++ Lesson_29/ViewController.swift
@@ -6,12 +6,38 @@
 //
 
 import UIKit
+import KeychainSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var label: UILabel!
+    
+//    создали объект KeyChain
+    private let keyChain = KeychainSwift()
+//    создаем ключ
+    private let keyChainKey = "keyChainKey"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        проверка на сохранение текста
+        getTextFromKeyChain()
+    }
+    
+    @IBAction private func buttonPressed() {
+        saveTextToKeyChain()
+        getTextFromKeyChain()
+    }
+    
+    private func saveTextToKeyChain() {
+        guard let text = textField.text else { return }
+        keyChain.set(text, forKey: keyChainKey, withAccess: nil)
+    }
+    
+    private func getTextFromKeyChain() {
+        guard let text = keyChain.get(keyChainKey) else { return }
+        label.text = text
     }
 
 
